4d34452d1d5815527beed17e78c3c9f20e46868c
Hérik Lemos <79945400+herikucl@users.noreply.github.com>
2021-06-20 14:52:00 +0000

    Criação das 3 funções pedidas juntamente com as formulas para cada calculo

--- desafio3.h
+++ desafio3.h
@@ -3,4 +3,17 @@ struct tInvestimento
 {
   int Periodo;
   float Montante,JurosAno;
+
+float MontanteFinalJS(){
+return (Montante*JurosAno*Periodo)+Montante;
+}
+
+float MontanteFinalJC(){
+return Montante*pow((1+JurosAno),Periodo);
+}
+
+float ConversaoJanoJmes(){
+return pow(1+JurosAno,(1.0/12.0))-1;
+}
+
 };
