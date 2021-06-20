193d9dc1b15067e2361455da32087a83b8fa94a6
Hérik Lemos <79945400+herikucl@users.noreply.github.com>
2021-06-20 14:53:18 +0000

    Funções extras

--- desafio3.h
+++ desafio3.h
@@ -1,19 +1,29 @@
 #include<math.h>
 struct tInvestimento
 {
-  int Periodo;
-  float Montante,JurosAno;
+int Periodo;
+float Montante,JurosAno;
 
-float MontanteFinalJS(){
-return (Montante*JurosAno*Periodo)+Montante;
-}
+  float MontanteFinalJS(int p){
+  return (Montante*JurosAno*p)+Montante;
+  }
+  float MontanteFinalJS(){
+  return (Montante*JurosAno*Periodo)+Montante;
+  }
 
-float MontanteFinalJC(){
-return Montante*pow((1+JurosAno),Periodo);
-}
 
-float ConversaoJanoJmes(){
-return pow(1+JurosAno,(1.0/12.0))-1;
-}
+  float MontanteFinalJC(int p){
+  return Montante*pow((1+JurosAno),p);
+  }
+  float MontanteFinalJC(){
+  return Montante*pow((1+JurosAno),Periodo);
+  }
+
+  float ConversaoJanoJmes(float j){
+  return pow(1+j,(1.0/12.0))-1;
+  }
+  float ConversaoJanoJmes(){
+  return pow(1+JurosAno,(1.0/12.0))-1;
+  }
 
 };
